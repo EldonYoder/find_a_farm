3140cb174d94d8769692b70b90b5c1ab8ddb7b48
Eldon Yoder <eldonyoder@gmail.com>
2014-06-13 13:51:47 -0400

    moved map, fixed devise stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,14 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-  end
+
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation)
+    end
+
+    devise_parameter_sanitizer.for(:account_update)do |u|
+      u.permit(:name, :email, :password, :password_confirmation, :current_password)
+    end
+
+  end 
 end
